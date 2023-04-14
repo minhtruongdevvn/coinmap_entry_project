a4b70ac180060c85e64b00d414538f8775e297fe
Minh Truong <minhtruongdevvn@gmail.com>
2023-04-14 20:20:19 +0700

    🎉 init(project): nest project init

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+
+@Module({
+  imports: [
+    TodoModule,
+    MongooseModule.forRoot(
+      'mongodb://coinmap_entry:pass@localhost:27017/coinmap_entry',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
